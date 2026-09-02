4b892a9bd643ba62082567c0406674c12e38900d
Konstantin Lepeshenkov <5447190+scale-tone@users.noreply.github.com>
2026-09-02 12:35:00 +0200

    Made OtelTapHttpProtobufReceiverSettings values optional

--- OtelTap/OtelTapHttpProtobufReceiverSettings.cs
+++ OtelTap/OtelTapHttpProtobufReceiverSettings.cs
@@ -5,7 +5,7 @@ namespace OtelTap
     /// Settings for OtelTapHttpProtobufReceiver
     /// </summary>
     /// <param name="HttpPort">HTTP port number to listen on</param>
-    /// <param name="ListenOnAllInterfaces">Should the receiver listen on all network interfaces</param>
+    /// <param name="ListenOnAllInterfaces">Should the receiver listen on all network interfaces. When false (default), listens only on loopback interface (127.0.0.1).</param>
     /// <param name="PrintTracesAsNdjson">Should traces be printed into standard output (as NDJSON)</param>
     /// <param name="PrintLogsAsNdjson">Should logs be printed into standard output (as NDJSON)</param>
     /// <param name="PrintMetricsAsNdjson">Should logs be printed into standard output (as NDJSON)</param>
@@ -16,14 +16,14 @@ namespace OtelTap
     public record OtelTapHttpProtobufReceiverSettings
     (
         int HttpPort,
-        bool ListenOnAllInterfaces,
-        bool PrintTracesAsNdjson,
-        bool PrintLogsAsNdjson,
-        bool PrintMetricsAsNdjson,
-        string? ReemitTracesToUrl,
-        string? ReemitLogsToUrl,
-        string? ReemitMetricsToUrl,
-        Action<string, Exception?>? Log
+        bool ListenOnAllInterfaces = false,
+        bool PrintTracesAsNdjson = false,
+        bool PrintLogsAsNdjson = false,
+        bool PrintMetricsAsNdjson = false,
+        string? ReemitTracesToUrl = null,
+        string? ReemitLogsToUrl = null,
+        string? ReemitMetricsToUrl = null,
+        Action<string, Exception?>? Log = null
     )
     {
     }
